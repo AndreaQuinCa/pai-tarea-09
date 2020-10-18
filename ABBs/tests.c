ab1d2e47924b395721a9a2ec72d5fd5a623dde12
Esteban <esteban.reyes@cimat.mx>
2020-10-18 16:46:03 -0500

    prueba de get

--- ABBs/tests.c
+++ ABBs/tests.c
@@ -16,15 +16,14 @@ int test_put_element(void){
   printf("*- Test Put Element -*\n");
   // Create RBT
   ABBNode *trial;
+
   trial = newABBNode(10,2);
-  
   if(isEmpty(trial) != 0){puts("ERROR"); return 0;}
   if(get(trial,10) != 2)   {puts("ERROR"); return 0;}
 
   put(trial,2,-10);
   if(isEmpty(trial) != 0){puts("ERROR"); return 0;}
   if(get(trial,2) != -10)   {puts("ERROR"); return 0;}
-
 
 
   puts("OK");
